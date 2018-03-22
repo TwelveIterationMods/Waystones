bd36a89b19da5e30a1f173a2941bb92f6cbeb226
blay09 <blay09@hotmail.de>
2018-03-22 03:51:47 +0100

    Fix global waystone checkbox not showing up on single player
    #82

--- src/main/java/net/blay09/mods/waystones/client/gui/GuiEditWaystone.java
+++ src/main/java/net/blay09/mods/waystones/client/gui/GuiEditWaystone.java
@@ -19,85 +19,86 @@ import java.io.IOException;
 
 public class GuiEditWaystone extends GuiContainer {
 
-	private final TileWaystone tileWaystone;
-	private GuiTextField textField;
-	private GuiButton btnDone;
-	private GuiCheckBox chkGlobal;
+    private final TileWaystone tileWaystone;
+    private GuiTextField textField;
+    private GuiButton btnDone;
+    private GuiCheckBox chkGlobal;
 
-	public GuiEditWaystone(TileWaystone tileWaystone) {
-		super(new ContainerEditWaystoneNameDummy());
-		this.tileWaystone = tileWaystone;
-	}
+    public GuiEditWaystone(TileWaystone tileWaystone) {
+        super(new ContainerEditWaystoneNameDummy());
+        this.tileWaystone = tileWaystone;
+    }
 
-	@Override
-	public void initGui() {
-		super.initGui();
-		String oldText = tileWaystone.getWaystoneName();
-		if(textField != null) {
-			oldText = textField.getText();
-		}
-		textField = new GuiTextField(2, fontRenderer, width / 2 - 100, height / 2 - 20, 200, 20);
-		textField.setMaxStringLength(128);
-		textField.setText(oldText);
-		textField.setFocused(true);
-		btnDone = new GuiButton(0, width / 2, height / 2 + 10, 100, 20, I18n.format("gui.done"));
-		buttonList.add(btnDone);
+    @Override
+    public void initGui() {
+        super.initGui();
+        String oldText = tileWaystone.getWaystoneName();
+        if (textField != null) {
+            oldText = textField.getText();
+        }
+        textField = new GuiTextField(2, fontRenderer, width / 2 - 100, height / 2 - 20, 200, 20);
+        textField.setMaxStringLength(128);
+        textField.setText(oldText);
+        textField.setFocused(true);
+        btnDone = new GuiButton(0, width / 2, height / 2 + 10, 100, 20, I18n.format("gui.done"));
+        buttonList.add(btnDone);
 
-		chkGlobal = new GuiCheckBox(1, width / 2 - 100, height / 2 + 15, " " + I18n.format("gui.waystones:editWaystone.isGlobal"), tileWaystone.isGlobal());
-		if(!WaystoneConfig.general.allowEveryoneGlobal && (!Minecraft.getMinecraft().player.capabilities.isCreativeMode || (FMLCommonHandler.instance().getMinecraftServerInstance() != null && FMLCommonHandler.instance().getMinecraftServerInstance().isSinglePlayer()))) {
-			chkGlobal.visible = false;
-		}
-		buttonList.add(chkGlobal);
+        chkGlobal = new GuiCheckBox(1, width / 2 - 100, height / 2 + 15, " " + I18n.format("gui.waystones:editWaystone.isGlobal"), tileWaystone.isGlobal());
+        if (!WaystoneConfig.general.allowEveryoneGlobal && (!Minecraft.getMinecraft().player.capabilities.isCreativeMode)) {
+            chkGlobal.visible = false;
+        }
+        buttonList.add(chkGlobal);
 
-		Keyboard.enableRepeatEvents(true);
-	}
+        Keyboard.enableRepeatEvents(true);
+    }
 
-	@Override
-	public void onGuiClosed() {
-		Keyboard.enableRepeatEvents(false);
-	}
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
+    }
 
-	@Override
-	protected void actionPerformed(GuiButton button) {
-		if(button == btnDone) {
-			NetworkHandler.channel.sendToServer(new MessageEditWaystone(tileWaystone.getPos(), textField.getText(), chkGlobal.isChecked()));
-			FMLClientHandler.instance().getClientPlayerEntity().closeScreen();
-		}
-	}
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        if (button == btnDone) {
+            NetworkHandler.channel.sendToServer(new MessageEditWaystone(tileWaystone.getPos(), textField.getText(), chkGlobal.isChecked()));
+            FMLClientHandler.instance().getClientPlayerEntity().closeScreen();
+        }
+    }
 
-	@Override
-	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-		super.mouseClicked(mouseX, mouseY, mouseButton);
-		textField.mouseClicked(mouseX, mouseY, mouseButton);
-	}
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        textField.mouseClicked(mouseX, mouseY, mouseButton);
+    }
 
-	@Override
-	protected void keyTyped(char typedChar, int keyCode) throws IOException {
-		if(keyCode == Keyboard.KEY_RETURN) {
-			actionPerformed(btnDone);
-			return;
-		}
-		if(textField.textboxKeyTyped(typedChar, keyCode)) {
-			return;
-		}
-		super.keyTyped(typedChar, keyCode);
-	}
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (keyCode == Keyboard.KEY_RETURN) {
+            actionPerformed(btnDone);
+            return;
+        }
+        if (textField.textboxKeyTyped(typedChar, keyCode)) {
+            return;
+        }
+        super.keyTyped(typedChar, keyCode);
+    }
 
-	@Override
-	public void updateScreen() {
-		textField.updateCursorCounter();
-	}
+    @Override
+    public void updateScreen() {
+        textField.updateCursorCounter();
+    }
 
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		drawWorldBackground(0);
-		super.drawScreen(mouseX, mouseY, partialTicks);
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawWorldBackground(0);
+        super.drawScreen(mouseX, mouseY, partialTicks);
 
-		fontRenderer.drawString(I18n.format("gui.waystones:editWaystone.enterName"), width / 2 - 100, height / 2 - 35, 0xFFFFFF);
-		textField.drawTextBox();
-	}
+        fontRenderer.drawString(I18n.format("gui.waystones:editWaystone.enterName"), width / 2 - 100, height / 2 - 35, 0xFFFFFF);
+        textField.drawTextBox();
+    }
 
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {}
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+    }
 
 }
