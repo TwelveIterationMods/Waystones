0ea17f36b0509fa4fb3a62c2581e79124ce24118
blay09 <blay09@hotmail.de>
2018-04-24 11:19:34 +0200

    Fix inventory teleport button not working when XP cost is disabled for it
    #102

--- src/main/java/net/blay09/mods/waystones/network/handler/HandlerTeleportToWaystone.java
+++ src/main/java/net/blay09/mods/waystones/network/handler/HandlerTeleportToWaystone.java
@@ -19,92 +19,96 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import javax.annotation.Nullable;
 
 public class HandlerTeleportToWaystone implements IMessageHandler<MessageTeleportToWaystone, IMessage> {
-	@Override
-	@Nullable
-	public IMessage onMessage(final MessageTeleportToWaystone message, final MessageContext ctx) {
-		NetworkHandler.getThreadListener(ctx).addScheduledTask(() -> {
-			EntityPlayer player = ctx.getServerHandler().player;
-			int dist = (int) Math.sqrt(player.getDistanceSqToCenter(message.getWaystone().getPos()));
-			int xpLevelCost = WaystoneConfig.general.blocksPerXPLevel > 0 ? MathHelper.clamp(dist / WaystoneConfig.general.blocksPerXPLevel, 0, WaystoneConfig.general.maximumXpCost) : 0;
-			ItemStack heldItem = player.getHeldItem(message.getHand());
-			switch (message.getWarpMode()) {
-				case INVENTORY_BUTTON:
-					if (!WaystoneConfig.general.teleportButtonReturnOnly && (!WaystoneConfig.general.inventoryButtonXpCost || player.experienceLevel < xpLevelCost)) {
-						return;
-					}
+    @Override
+    @Nullable
+    public IMessage onMessage(final MessageTeleportToWaystone message, final MessageContext ctx) {
+        NetworkHandler.getThreadListener(ctx).addScheduledTask(() -> {
+            EntityPlayer player = ctx.getServerHandler().player;
+            int dist = (int) Math.sqrt(player.getDistanceSqToCenter(message.getWaystone().getPos()));
+            int xpLevelCost = WaystoneConfig.general.blocksPerXPLevel > 0 ? MathHelper.clamp(dist / WaystoneConfig.general.blocksPerXPLevel, 0, WaystoneConfig.general.maximumXpCost) : 0;
+            ItemStack heldItem = player.getHeldItem(message.getHand());
+            switch (message.getWarpMode()) {
+                case INVENTORY_BUTTON:
+                    if (!WaystoneConfig.general.teleportButton || WaystoneConfig.general.teleportButtonReturnOnly) {
+                        return;
+                    }
 
-					if (!WaystoneConfig.general.teleportButton || WaystoneConfig.general.teleportButtonReturnOnly || !PlayerWaystoneHelper.canFreeWarp(ctx.getServerHandler().player)) {
-						return;
-					}
+                    if (WaystoneConfig.general.inventoryButtonXpCost && player.experienceLevel < xpLevelCost) {
+                        return;
+                    }
 
-					break;
-				case WARP_SCROLL:
-					if (heldItem.isEmpty() || heldItem.getItem() != Waystones.itemWarpScroll) {
-						return;
-					}
+                    if (!PlayerWaystoneHelper.canFreeWarp(ctx.getServerHandler().player)) {
+                        return;
+                    }
 
-					break;
-				case WARP_STONE:
-					if (WaystoneConfig.general.warpStoneXpCost && player.experienceLevel < xpLevelCost) {
-						return;
-					}
+                    break;
+                case WARP_SCROLL:
+                    if (heldItem.isEmpty() || heldItem.getItem() != Waystones.itemWarpScroll) {
+                        return;
+                    }
 
-					if (heldItem.isEmpty() || heldItem.getItem() != Waystones.itemWarpStone) {
-						return;
-					}
+                    break;
+                case WARP_STONE:
+                    if (WaystoneConfig.general.warpStoneXpCost && player.experienceLevel < xpLevelCost) {
+                        return;
+                    }
 
-					if (!PlayerWaystoneHelper.canUseWarpStone(player)) {
-						return;
-					}
+                    if (heldItem.isEmpty() || heldItem.getItem() != Waystones.itemWarpStone) {
+                        return;
+                    }
 
-					break;
-				case WAYSTONE:
-					if (WaystoneConfig.general.waystoneXpCost && player.experienceLevel < xpLevelCost) {
-						return;
-					}
+                    if (!PlayerWaystoneHelper.canUseWarpStone(player)) {
+                        return;
+                    }
 
-					WaystoneEntry fromWaystone = message.getFromWaystone();
-					if (fromWaystone == null || WaystoneManager.getWaystoneInWorld(fromWaystone) == null) {
-						return;
-					}
+                    break;
+                case WAYSTONE:
+                    if (WaystoneConfig.general.waystoneXpCost && player.experienceLevel < xpLevelCost) {
+                        return;
+                    }
 
-					break;
-			}
+                    WaystoneEntry fromWaystone = message.getFromWaystone();
+                    if (fromWaystone == null || WaystoneManager.getWaystoneInWorld(fromWaystone) == null) {
+                        return;
+                    }
 
-			if (WaystoneManager.teleportToWaystone(ctx.getServerHandler().player, message.getWaystone())) {
-				boolean shouldCooldown = !(message.getWaystone().isGlobal() && WaystoneConfig.general.globalNoCooldown);
-				switch (message.getWarpMode()) {
-					case INVENTORY_BUTTON:
-						if (shouldCooldown) {
-							PlayerWaystoneHelper.setLastFreeWarp(ctx.getServerHandler().player, System.currentTimeMillis());
-						}
+                    break;
+            }
 
-						if (WaystoneConfig.general.inventoryButtonXpCost) {
-							player.addExperienceLevel(-xpLevelCost);
-						}
-						break;
-					case WARP_SCROLL:
-						heldItem.shrink(1);
-						break;
-					case WARP_STONE:
-						if (WaystoneConfig.general.warpStoneXpCost) {
-							player.addExperienceLevel(-xpLevelCost);
-						}
+            if (WaystoneManager.teleportToWaystone(ctx.getServerHandler().player, message.getWaystone())) {
+                boolean shouldCooldown = !(message.getWaystone().isGlobal() && WaystoneConfig.general.globalNoCooldown);
+                switch (message.getWarpMode()) {
+                    case INVENTORY_BUTTON:
+                        if (shouldCooldown) {
+                            PlayerWaystoneHelper.setLastFreeWarp(ctx.getServerHandler().player, System.currentTimeMillis());
+                        }
 
-						if (shouldCooldown) {
-							PlayerWaystoneHelper.setLastWarpStoneUse(ctx.getServerHandler().player, System.currentTimeMillis());
-						}
-						break;
-					case WAYSTONE:
-						if (WaystoneConfig.general.waystoneXpCost) {
-							player.addExperienceLevel(-xpLevelCost);
-						}
-						break;
-				}
-			}
+                        if (WaystoneConfig.general.inventoryButtonXpCost) {
+                            player.addExperienceLevel(-xpLevelCost);
+                        }
+                        break;
+                    case WARP_SCROLL:
+                        heldItem.shrink(1);
+                        break;
+                    case WARP_STONE:
+                        if (WaystoneConfig.general.warpStoneXpCost) {
+                            player.addExperienceLevel(-xpLevelCost);
+                        }
 
-			WaystoneManager.sendPlayerWaystones(ctx.getServerHandler().player);
-		});
-		return null;
-	}
+                        if (shouldCooldown) {
+                            PlayerWaystoneHelper.setLastWarpStoneUse(ctx.getServerHandler().player, System.currentTimeMillis());
+                        }
+                        break;
+                    case WAYSTONE:
+                        if (WaystoneConfig.general.waystoneXpCost) {
+                            player.addExperienceLevel(-xpLevelCost);
+                        }
+                        break;
+                }
+            }
+
+            WaystoneManager.sendPlayerWaystones(ctx.getServerHandler().player);
+        });
+        return null;
+    }
 }
