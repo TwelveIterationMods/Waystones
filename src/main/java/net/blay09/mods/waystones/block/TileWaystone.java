9eef82a66390111d7cd77310bd77e85a356a3a7b
blay09 <blay09@hotmail.de>
2018-04-27 10:40:29 +0200

    Add %RANDOM% special waystone name that will cause the name to only be generated when the waystone is loaded for the next time ... probably
    #93

--- src/main/java/net/blay09/mods/waystones/block/TileWaystone.java
+++ src/main/java/net/blay09/mods/waystones/block/TileWaystone.java
@@ -17,116 +17,118 @@ import java.util.UUID;
 
 public class TileWaystone extends TileEntity {
 
-	private boolean isDummy;
-	private String waystoneName = "";
-	private UUID owner;
-	private boolean isGlobal;
+    private boolean isDummy;
+    private String waystoneName = "";
+    private UUID owner;
+    private boolean isGlobal;
 
-	public TileWaystone() {
-	}
+    public TileWaystone() {
+    }
 
-	public TileWaystone(boolean isDummy) {
-		this.isDummy = isDummy;
-	}
+    public TileWaystone(boolean isDummy) {
+        this.isDummy = isDummy;
+    }
 
-	@Override
-	public NBTTagCompound writeToNBT(NBTTagCompound tagCompound) {
-		super.writeToNBT(tagCompound);
-		tagCompound.setBoolean("IsDummy", isDummy);
-		if(!isDummy) {
-			tagCompound.setString("WaystoneName", waystoneName);
-			if (owner != null) {
-				tagCompound.setTag("Owner", NBTUtil.createUUIDTag(owner));
-			}
-			tagCompound.setBoolean("IsGlobal", isGlobal);
-		}
-		return tagCompound;
-	}
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound tagCompound) {
+        super.writeToNBT(tagCompound);
+        tagCompound.setBoolean("IsDummy", isDummy);
+        if (!isDummy) {
+            if (!waystoneName.equals("%RANDOM%")) {
+                tagCompound.setString("WaystoneName", waystoneName);
+            }
+            if (owner != null) {
+                tagCompound.setTag("Owner", NBTUtil.createUUIDTag(owner));
+            }
+            tagCompound.setBoolean("IsGlobal", isGlobal);
+        }
+        return tagCompound;
+    }
 
-	@Override
-	public void readFromNBT(NBTTagCompound tagCompound) {
-		super.readFromNBT(tagCompound);
-		isDummy = tagCompound.getBoolean("IsDummy");
-		if(!isDummy) {
-			waystoneName = tagCompound.getString("WaystoneName");
-			if (tagCompound.hasKey("Owner")) {
-				owner = NBTUtil.getUUIDFromTag(tagCompound.getCompoundTag("Owner"));
-			}
-			isGlobal = tagCompound.getBoolean("IsGlobal");
-		}
-	}
+    @Override
+    public void readFromNBT(NBTTagCompound tagCompound) {
+        super.readFromNBT(tagCompound);
+        isDummy = tagCompound.getBoolean("IsDummy");
+        if (!isDummy) {
+            waystoneName = tagCompound.getString("WaystoneName");
+            if (tagCompound.hasKey("Owner")) {
+                owner = NBTUtil.getUUIDFromTag(tagCompound.getCompoundTag("Owner"));
+            }
+            isGlobal = tagCompound.getBoolean("IsGlobal");
+        }
+    }
 
-	@Override
-	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
-		super.onDataPacket(net, pkt);
-		generateNameIfNecessary();
-		readFromNBT(pkt.getNbtCompound());
-	}
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        super.onDataPacket(net, pkt);
+        generateNameIfNecessary();
+        readFromNBT(pkt.getNbtCompound());
+    }
 
-	@Override
-	public NBTTagCompound getUpdateTag() {
-		generateNameIfNecessary();
-		return writeToNBT(new NBTTagCompound());
-	}
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        generateNameIfNecessary();
+        return writeToNBT(new NBTTagCompound());
+    }
 
-	private void generateNameIfNecessary() {
-		if(waystoneName.isEmpty()) {
-			waystoneName = NameGenerator.get(world).getName(world.getBiome(pos), world.rand);
-		}
-	}
+    private void generateNameIfNecessary() {
+        if (waystoneName.isEmpty()) {
+            waystoneName = NameGenerator.get(world).getName(world.getBiome(pos), world.rand);
+        }
+    }
 
-	@Nullable
-	@Override
-	public SPacketUpdateTileEntity getUpdatePacket() {
-		return new SPacketUpdateTileEntity(pos, 0, getUpdateTag());
-	}
+    @Nullable
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(pos, 0, getUpdateTag());
+    }
 
-	public String getWaystoneName() {
-		return waystoneName;
-	}
+    public String getWaystoneName() {
+        return waystoneName;
+    }
 
-	public boolean isOwner(EntityPlayer player) {
-		return owner == null || player.getGameProfile().getId().equals(owner) || player.capabilities.isCreativeMode;
-	}
+    public boolean isOwner(EntityPlayer player) {
+        return owner == null || player.getGameProfile().getId().equals(owner) || player.capabilities.isCreativeMode;
+    }
 
-	public void setWaystoneName(String waystoneName) {
-		this.waystoneName = waystoneName;
-		IBlockState state = world.getBlockState(pos);
-		world.markAndNotifyBlock(pos, world.getChunkFromBlockCoords(pos), state, state, 3);
-		markDirty();
-	}
+    public void setWaystoneName(String waystoneName) {
+        this.waystoneName = waystoneName;
+        IBlockState state = world.getBlockState(pos);
+        world.markAndNotifyBlock(pos, world.getChunkFromBlockCoords(pos), state, state, 3);
+        markDirty();
+    }
 
-	@Override
-	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
-		return oldState.getBlock() != newSate.getBlock();
-	}
+    @Override
+    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+        return oldState.getBlock() != newSate.getBlock();
+    }
 
-	@Override
-	public AxisAlignedBB getRenderBoundingBox() {
-		return new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 2, pos.getZ() + 1);
-	}
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 2, pos.getZ() + 1);
+    }
 
-	public void setOwner(EntityPlayer owner) {
-		this.owner = owner.getGameProfile().getId();
-		markDirty();
-	}
+    public void setOwner(EntityPlayer owner) {
+        this.owner = owner.getGameProfile().getId();
+        markDirty();
+    }
 
-	public boolean isGlobal() {
-		return isGlobal;
-	}
+    public boolean isGlobal() {
+        return isGlobal;
+    }
 
-	public void setGlobal(boolean isGlobal) {
-		this.isGlobal = isGlobal;
-		markDirty();
-	}
+    public void setGlobal(boolean isGlobal) {
+        this.isGlobal = isGlobal;
+        markDirty();
+    }
 
-	public TileWaystone getParent() {
-		if (isDummy) {
-			TileEntity tileBelow = world.getTileEntity(pos.down());
-			if (tileBelow instanceof TileWaystone) {
-				return (TileWaystone) tileBelow;
-			}
-		}
-		return this;
-	}
+    public TileWaystone getParent() {
+        if (isDummy) {
+            TileEntity tileBelow = world.getTileEntity(pos.down());
+            if (tileBelow instanceof TileWaystone) {
+                return (TileWaystone) tileBelow;
+            }
+        }
+        return this;
+    }
 }
