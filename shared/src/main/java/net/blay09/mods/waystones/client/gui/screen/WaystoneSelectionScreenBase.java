83157eea97422f3d69ffa11d870dba1c3901dc5a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-04-30 12:54:44 +0200

    fix: Fix book icon for renaming waystone being positioned incorrectly

--- shared/src/main/java/net/blay09/mods/waystones/client/gui/screen/WaystoneSelectionScreenBase.java
+++ shared/src/main/java/net/blay09/mods/waystones/client/gui/screen/WaystoneSelectionScreenBase.java
@@ -286,14 +286,13 @@ public abstract class WaystoneSelectionScreenBase extends AbstractContainerScree
         drawString(poseStack, fontRenderer, fullText, x - fullWidth / 2, y, 0xFFFFFF);
 
         if (isLocationHeaderHovered && waystoneEditPermissions == WaystoneEditPermissions.ALLOW) {
-            PoseStack modelViewStack = RenderSystem.getModelViewStack();
-            modelViewStack.pushPose();
-            modelViewStack.translate(x + fullWidth / 2f + 4, y, 0f);
+            poseStack.pushPose();
             float scale = 0.5f;
-            modelViewStack.scale(scale, scale, scale);
+            poseStack.translate(x + fullWidth / 2f + 4, y, 0f);
+            poseStack.scale(scale, scale, scale);
             RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_BLOCKS);
             Minecraft.getInstance().getItemRenderer().renderAndDecorateItem(poseStack, new ItemStack(Items.WRITABLE_BOOK), 0, 0);
-            modelViewStack.popPose();
+            poseStack.popPose();
         }
     }
 
