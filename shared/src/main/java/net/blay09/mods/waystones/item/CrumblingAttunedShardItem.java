8328ac78d2cf4d65de7b5da83074808e0313f978
edralzar <edralzar@gmail.com>
2023-11-11 23:04:33 +0100

    Fix tooltip texts, simplify CrumblingAttunedShardItem with extends

--- shared/src/main/java/net/blay09/mods/waystones/item/CrumblingAttunedShardItem.java
+++ shared/src/main/java/net/blay09/mods/waystones/item/CrumblingAttunedShardItem.java
@@ -1,37 +1,22 @@
 package net.blay09.mods.waystones.item;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.waystones.api.IAttunementItem;
 import net.blay09.mods.waystones.api.IWaystone;
-import net.blay09.mods.waystones.block.WarpPlateBlock;
-import net.blay09.mods.waystones.core.WaystoneProxy;
 import net.blay09.mods.waystones.menu.WarpPlateContainer;
 import net.minecraft.ChatFormatting;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtUtils;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Objects;
 
-public class CrumblingAttunedShardItem extends Item implements IAttunementItem {
+public class CrumblingAttunedShardItem extends AttunedShardItem {
 
     public CrumblingAttunedShardItem(Properties properties) {
         super(properties.stacksTo(4));
-    }
-
-    @Override
-    public boolean isFoil(ItemStack itemStack) {
-        IWaystone waystoneAttunedTo = getWaystoneAttunedTo(null, itemStack);
-        return waystoneAttunedTo != null && waystoneAttunedTo.isValid();
     }
 
     @Override
@@ -39,57 +24,20 @@ public class CrumblingAttunedShardItem extends Item implements IAttunementItem {
         super.appendHoverText(stack, world, list, flag);
 
         IWaystone attunedWarpPlate = getWaystoneAttunedTo(null, stack);
-        if (attunedWarpPlate == null || !attunedWarpPlate.isValid()) {
-            var textComponent = Component.translatable("tooltip.waystones.attuned_shard.attunement_lost");
-            textComponent.withStyle(ChatFormatting.GRAY);
-            list.add(textComponent);
-            return;
-        }
+        if (attunedWarpPlate != null && attunedWarpPlate.isValid()) {
+            var textComponent = Component.translatable("tooltip.waystones.attuned_shard.attunement_crumbling");
+            textComponent.withStyle(ChatFormatting.WHITE).withStyle(ChatFormatting.ITALIC);
 
-        list.add(WarpPlateBlock.getGalacticName(attunedWarpPlate));
-
-        Player player = Balm.getProxy().getClientPlayer();
-        if (player != null && player.containerMenu instanceof WarpPlateContainer) {
-            IWaystone currentWarpPlate = ((WarpPlateContainer) player.containerMenu).getWaystone();
-            if (attunedWarpPlate.getWaystoneUid().equals(currentWarpPlate.getWaystoneUid())) {
-                list.add(Component.translatable("tooltip.waystones.attuned_shard.move_to_other_warp_plate"));
-            } else {
-                list.add(Component.translatable("tooltip.waystones.attuned_shard.plug_into_warp_plate_or_use"));
+            Player player = Balm.getProxy().getClientPlayer();
+            if (player != null && player.containerMenu instanceof WarpPlateContainer wpc) {
+                if (!attunedWarpPlate.getWaystoneUid().equals(wpc.getWaystone().getWaystoneUid())) {
+                    list.add(textComponent);
+                }
             }
-        } else {
-            list.add(Component.translatable("tooltip.waystones.attuned_shard.plug_into_warp_plate_or_use"));
-        }
-
-        list.add(Component.translatable("tooltip.waystones.attuned_shard.attunement_crumbling"));
-    }
-
-    @Nullable
-    @Override
-    public IWaystone getWaystoneAttunedTo(MinecraftServer server, ItemStack itemStack) {
-        CompoundTag compound = itemStack.getTag();
-        if (compound != null && compound.contains("AttunedToWaystone", Tag.TAG_INT_ARRAY)) {
-            return new WaystoneProxy(server, NbtUtils.loadUUID(Objects.requireNonNull(compound.get("AttunedToWaystone"))));
-        }
-
-        return null;
-    }
-
-    @Override
-    public void setWaystoneAttunedTo(ItemStack itemStack, @Nullable IWaystone waystone) {
-        CompoundTag tagCompound = itemStack.getTag();
-        if (tagCompound == null) {
-            tagCompound = new CompoundTag();
-            itemStack.setTag(tagCompound);
-        }
-
-        if (waystone != null) {
-            tagCompound.put("AttunedToWaystone", NbtUtils.createUUID(waystone.getWaystoneUid()));
-        } else {
-            tagCompound.remove("AttunedToWaystone");
+            else {
+                list.add(textComponent);
+            }
         }
     }
-
-    //TODO find a way to bind the crumbling shard(s) to a warp plate so that it overrides its destination for x number of teleports
-    //(contrary to just placing the shard(s) inside a plate which already has a more permanent shard, which would randomly select destination)
 
 }
