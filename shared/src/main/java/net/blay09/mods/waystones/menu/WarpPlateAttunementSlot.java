62e234094004856987a705be81dee8f750c377ab
Simon B <edralzar@gmail.com>
2023-11-12 23:35:53 +0100

    fix: Limit the Warp Plate's center slot max stack size to 1 (#746)
    Also prevents hoppers from inserting more items in that
    slot if it already has an item, and amend quickMoveStack
    to also take that limitation into consideration.
    
    Fixes gh-739

--- shared/src/main/java/net/blay09/mods/waystones/menu/WarpPlateAttunementSlot.java
+++ shared/src/main/java/net/blay09/mods/waystones/menu/WarpPlateAttunementSlot.java
@@ -3,6 +3,7 @@ package net.blay09.mods.waystones.menu;
 import net.blay09.mods.waystones.block.entity.WarpPlateBlockEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 
 public class WarpPlateAttunementSlot extends Slot {
     private final WarpPlateBlockEntity warpPlate;
@@ -15,5 +16,13 @@ public class WarpPlateAttunementSlot extends Slot {
     @Override
     public boolean mayPickup(Player player) {
         return warpPlate.isCompletedFirstAttunement() && super.mayPickup(player);
+    }
+
+    @Override
+    public int getMaxStackSize(ItemStack stack) {
+        if (this.getContainerSlot() == 0) {
+            return 1;
+        }
+        return stack.getMaxStackSize();
     }
 }
