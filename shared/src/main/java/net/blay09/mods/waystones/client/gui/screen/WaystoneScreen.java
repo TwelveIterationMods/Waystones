bd3c37752b9c2e9af40693223e44c67c07206804
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-27 14:41:16 +0100

    feat: Only focus text field if name is empty to prevent accidental changes

--- shared/src/main/java/net/blay09/mods/waystones/client/gui/screen/WaystoneScreen.java
+++ shared/src/main/java/net/blay09/mods/waystones/client/gui/screen/WaystoneScreen.java
@@ -18,7 +18,6 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.Slot;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
@@ -60,7 +59,7 @@ public class WaystoneScreen extends AbstractContainerScreen<WaystoneMenu> {
         textField.setValue(oldText);
         textField.setEditable(menu.canEdit());
         addRenderableWidget(textField);
-        if (menu.canEdit()) {
+        if (menu.canEdit() && oldText.isEmpty()) {
             setInitialFocus(textField);
         }
 
