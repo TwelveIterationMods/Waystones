e48d2ccbbf9cb38d7210812c7a972fbb88b0d6cb
Simon B <edralzar@gmail.com>
2024-02-27 17:50:13 +0100

    Fix editability of sharestone with new UI (#794)
    * Fix canEdit of sharestone in new UI, display network in tooltip
    
    * fix: Fix sharestone visibilities
    
    ---------
    
    Co-authored-by: BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>

--- shared/src/main/java/net/blay09/mods/waystones/api/WaystoneVisibility.java
+++ shared/src/main/java/net/blay09/mods/waystones/api/WaystoneVisibility.java
@@ -1,7 +1,69 @@
 package net.blay09.mods.waystones.api;
 
+import net.minecraft.resources.ResourceLocation;
+
 public enum WaystoneVisibility {
     ACTIVATION,
     GLOBAL,
-    SHARD_ONLY
+    SHARD_ONLY,
+    SHARESTONE,
+    WHITE_SHARESTONE,
+    ORANGE_SHARESTONE,
+    MAGENTA_SHARESTONE,
+    LIGHT_BLUE_SHARESTONE,
+    YELLOW_SHARESTONE,
+    LIME_SHARESTONE,
+    PINK_SHARESTONE,
+    GRAY_SHARESTONE,
+    LIGHT_GRAY_SHARESTONE,
+    CYAN_SHARESTONE,
+    PURPLE_SHARESTONE,
+    BLUE_SHARESTONE,
+    BROWN_SHARESTONE,
+    GREEN_SHARESTONE,
+    RED_SHARESTONE,
+    BLACK_SHARESTONE;
+
+    public static WaystoneVisibility fromWaystoneType(ResourceLocation waystoneType) {
+        if (WaystoneTypes.isSharestone(waystoneType)) {
+            return switch (waystoneType.getPath()) {
+                case "white_sharestone" -> WaystoneVisibility.WHITE_SHARESTONE;
+                case "orange_sharestone" -> WaystoneVisibility.ORANGE_SHARESTONE;
+                case "magenta_sharestone" -> WaystoneVisibility.MAGENTA_SHARESTONE;
+                case "light_blue_sharestone" -> WaystoneVisibility.LIGHT_BLUE_SHARESTONE;
+                case "yellow_sharestone" -> WaystoneVisibility.YELLOW_SHARESTONE;
+                case "lime_sharestone" -> WaystoneVisibility.LIME_SHARESTONE;
+                case "pink_sharestone" -> WaystoneVisibility.PINK_SHARESTONE;
+                case "gray_sharestone" -> WaystoneVisibility.GRAY_SHARESTONE;
+                case "light_gray_sharestone" -> WaystoneVisibility.LIGHT_GRAY_SHARESTONE;
+                case "cyan_sharestone" -> WaystoneVisibility.CYAN_SHARESTONE;
+                case "purple_sharestone" -> WaystoneVisibility.PURPLE_SHARESTONE;
+                case "blue_sharestone" -> WaystoneVisibility.BLUE_SHARESTONE;
+                case "brown_sharestone" -> WaystoneVisibility.BROWN_SHARESTONE;
+                case "green_sharestone" -> WaystoneVisibility.GREEN_SHARESTONE;
+                case "red_sharestone" -> WaystoneVisibility.RED_SHARESTONE;
+                case "black_sharestone" -> WaystoneVisibility.BLACK_SHARESTONE;
+                default -> WaystoneVisibility.SHARESTONE;
+            };
+        } else if (waystoneType.equals(WaystoneTypes.WARP_PLATE)) {
+            return WaystoneVisibility.SHARD_ONLY;
+        } else if (waystoneType.equals(WaystoneTypes.LANDING_STONE)) {
+            return WaystoneVisibility.SHARD_ONLY;
+        } else {
+            return WaystoneVisibility.ACTIVATION;
+        }
+    }
+
+    public int getIconX() {
+        return 176;
+    }
+
+    public int getIconY() {
+        return switch (this) {
+            case ACTIVATION -> 32;
+            case GLOBAL -> 50;
+            case SHARD_ONLY -> 68;
+            default -> 86;
+        };
+    }
 }
