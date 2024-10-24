6fdaca06c3daebfdaaceb0427a3a08f975402c31
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-24 13:07:27 +0200

    fix: Fix visibility being locked to global if defaultVisibility is set to global

--- common/src/main/java/net/blay09/mods/waystones/core/WaystoneVisibilities.java
+++ common/src/main/java/net/blay09/mods/waystones/core/WaystoneVisibilities.java
@@ -2,6 +2,7 @@ package net.blay09.mods.waystones.core;
 
 import net.blay09.mods.waystones.api.Waystone;
 import net.blay09.mods.waystones.api.WaystoneVisibility;
+import net.blay09.mods.waystones.config.WaystonesConfig;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.ArrayList;
@@ -10,11 +11,17 @@ import java.util.List;
 public class WaystoneVisibilities {
     public static List<WaystoneVisibility> getVisibilityOptions(Player player, Waystone waystone) {
         final var result = new ArrayList<WaystoneVisibility>();
+        final var defaultVisibility = WaystonesConfig.getActive().general.defaultVisibility;
+        result.add(defaultVisibility);
         final var baseVisibility = WaystoneVisibility.fromWaystoneType(waystone.getWaystoneType());
-        result.add(baseVisibility);
+        if (!result.contains(baseVisibility)) {
+            result.add(baseVisibility);
+        }
         if (baseVisibility == WaystoneVisibility.ACTIVATION) {
             if (WaystonePermissionManager.isAllowedVisibility(WaystoneVisibility.GLOBAL) || WaystonePermissionManager.skipsPermissions(player)) {
-                result.add(WaystoneVisibility.GLOBAL);
+                if (!result.contains(WaystoneVisibility.GLOBAL)) {
+                    result.add(WaystoneVisibility.GLOBAL);
+                }
             }
         }
         return result;
